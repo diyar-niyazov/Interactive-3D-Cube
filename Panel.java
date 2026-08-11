be4afbd9ca6ad48c443c792c973976d2e0d4a144
= <Diyar.Niyazov08@gmail.com>
2026-08-11 11:45:57 -0400

    Added x-axis rotation and improved keyboard controls

--- Panel.java
+++ Panel.java
@@ -9,13 +9,22 @@ import javax.swing.Timer;
 public class Panel extends JPanel implements KeyListener {
     private static final double CUBE_SIDE_LENGTH = 1;
     // distance from the camera to the projection plane
-    private static final int Z_NEAR = 3;
     private static final int FPS = 60;
 
-    private final int screenWidth, screenHeight;
+    private boolean showPoints = true;
 
-    private double angle = 0;
-    private double angleIncrement = Math.PI / FPS / 2;
+    private final int screenWidth, screenHeight;
+    private double Z_NEAR = 3;
+
+    private boolean initialRotatingX = false;
+    private boolean rotatingX = false;
+    private double xAngle = 0;
+    private double xAngleIncrement = Math.PI / FPS / 2;
+
+    private boolean initialRotatingY = true;
+    private boolean rotatingY = true;
+    private double yAngle = 0;
+    private double yAngleIncrement = -Math.PI / FPS / 2;
 
     private double[][] initialVertices;
     private double[][] rotatedVertices;
@@ -51,21 +60,30 @@ public class Panel extends JPanel implements KeyListener {
         rotatedVertices = initialVertices;
 
         Timer timer = new Timer(16, e -> {
-            angle += angleIncrement;
-            rotatedVertices = rotateY(initialVertices, angle);
+            rotatedVertices = initialVertices;
+            if (rotatingX) {
+                xAngle += xAngleIncrement;
+            }
+            if (rotatingY) {
+                yAngle += yAngleIncrement;
+            }
+            rotatedVertices = rotateY(rotateX(initialVertices, xAngle), yAngle);
+
             projectedVertices = project(rotatedVertices);
             translatedVertices = translate(projectedVertices);
             repaint();
         });
         timer.start();
+
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-
-        g.setColor(Color.WHITE);
-        drawPoints(g, translatedVertices);
+        if (showPoints) {
+            g.setColor(Color.WHITE);
+            drawPoints(g, translatedVertices);
+        }
         g.setColor(Color.GREEN);
         drawLines(g, translatedVertices);
     }
@@ -110,22 +128,40 @@ public class Panel extends JPanel implements KeyListener {
         }
     }
 
-    public double[][] rotateY(double[][] vertices, double angle) {
-        double cos = Math.cos(angle);
-        double sin = Math.sin(angle);
+    public double[][] rotateY(double[][] vertices, double yAngle) {
+        double cos = Math.cos(yAngle);
+        double sin = Math.sin(yAngle);
 
-        double[][] result = new double[vertices.length][3];
+        double[][] rotatedVertices = new double[vertices.length][3];
 
         for (int i = 0; i < vertices.length; i++) {
             double x = vertices[i][0];
             double y = vertices[i][1];
             double z = vertices[i][2];
 
-            result[i][0] = x * cos + z * sin;
-            result[i][1] = y;
-            result[i][2] = -x * sin + z * cos;
+            rotatedVertices[i][0] = x * cos + z * sin;
+            rotatedVertices[i][1] = y;
+            rotatedVertices[i][2] = -x * sin + z * cos;
         }
-        return result;
+        return rotatedVertices;
+    }
+
+    public double[][] rotateX(double[][] vertices, double xAngle) {
+        double cos = Math.cos(xAngle);
+        double sin = Math.sin(xAngle);
+
+        double[][] rotatedVertices = new double[vertices.length][3];
+
+        for (int i = 0; i < vertices.length; i++) {
+            double x = vertices[i][0];
+            double y = vertices[i][1];
+            double z = vertices[i][2];
+
+            rotatedVertices[i][0] = x;
+            rotatedVertices[i][1] = y * cos - z * sin;
+            rotatedVertices[i][2] = y * sin + z * cos;
+        }
+        return rotatedVertices;
     }
 
     public double[][] project(double[][] vertices) {
@@ -175,23 +211,71 @@ public class Panel extends JPanel implements KeyListener {
 
     @Override
     public void keyTyped(KeyEvent ke) {
-        throw new UnsupportedOperationException("Not supported yet.");
     }
 
     @Override
-    public void keyPressed(KeyEvent ke) {
-        boolean spinningClockwise = angleIncrement > 0;
-        if (ke.getKeyCode() == KeyEvent.VK_RIGHT) {
-            if (spinningClockwise)
-                angleIncrement = -1 * angleIncrement;
-        } else if (ke.getKeyCode() == KeyEvent.VK_LEFT) {
-            if (!spinningClockwise)
-                angleIncrement = -1 * angleIncrement;
+    public void keyPressed(KeyEvent ke) {        
+        int keyCode = ke.getKeyCode();
+
+        if (keyCode == KeyEvent.VK_SPACE) {
+            if (rotatingX || rotatingY) {
+                initialRotatingX = rotatingX;
+                initialRotatingY = rotatingY;
+                rotatingX = false;
+                rotatingY = false;
+            } else {
+                rotatingX = initialRotatingX;
+                rotatingY = initialRotatingY;
+            }
+        }
+
+        boolean rotatingLeft = yAngleIncrement > 0;
+        if (keyCode == KeyEvent.VK_RIGHT) {
+            if (!rotatingY) {
+                yAngleIncrement = -1 * Math.abs(yAngleIncrement);
+                rotatingY = true;
+            } else if (rotatingY && rotatingLeft) {
+                rotatingY = false;
+            }
+        } else if (keyCode == KeyEvent.VK_LEFT) {
+            if (!rotatingY) {
+                yAngleIncrement = Math.abs(yAngleIncrement);
+                rotatingY = true;
+            } else if (rotatingY && !rotatingLeft) {
+                rotatingY = false;
+            }
+        }
+
+        boolean rotatingUp = xAngleIncrement > 0;
+        if (keyCode == KeyEvent.VK_UP) {
+            if (!rotatingX) {
+                xAngleIncrement = Math.abs(xAngleIncrement);
+                rotatingX = true;
+            } else if (rotatingX && !rotatingUp) {
+                rotatingX = false;
+            }
+        } else if (keyCode == KeyEvent.VK_DOWN) {
+            if (!rotatingX) {
+                xAngleIncrement = -1 * Math.abs(xAngleIncrement);
+                rotatingX = true;
+            } else if (rotatingX && rotatingUp) {
+                rotatingX = false;
+            }
+        }
+
+        double MAX_Z_NEAR = 2.5;
+        double MIN_Z_NEAR = 0.1;
+        double Z_NEAR_INCREMENT = 0.1;
+        if (keyCode == KeyEvent.VK_W) {
+            if (Z_NEAR > MAX_Z_NEAR)
+                Z_NEAR -= Z_NEAR_INCREMENT;
+        } else if (keyCode == KeyEvent.VK_S) {
+            if (Z_NEAR > MIN_Z_NEAR)
+                Z_NEAR += Z_NEAR_INCREMENT;
         }
     }
 
     @Override
     public void keyReleased(KeyEvent ke) {
-
     }
 }
